1793a8ae885b007555ada4774de31525e676ddf4
dptorri <dptorri@gmail.com>
2021-11-11 00:08:05 +0100

    2.2 POST save() Person method increments persons list

--- src/main/java/example/controller/PersonController.java
+++ src/main/java/example/controller/PersonController.java
@@ -1,5 +1,25 @@
 package example.controller;
 
+import io.micronaut.http.annotation.Body;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import example.model.Person;
+import io.micronaut.http.annotation.Post;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Controller("persons")
 public class PersonController {
 
+    List<Person> persons = new ArrayList<>();
+
+    @Post
+    public Person save(@Body Person person) {
+        person.setId(persons.size() + 1);
+        persons.add(person);
+
+        return person;
+    }
 }
